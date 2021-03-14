9c30fefee955b88dee4c0231cd4ebc1c738bd5be
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2021-03-14 20:30:49 +0000

    [tests] Add a Donator spec

---
+++ spec/models/donator_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.describe Donator do
+  subject(:donator) { FactoryBot.create(:donator) }
+
+  describe "#total_donations" do
+    before do
+      FactoryBot.create(:donation,
+        donator: donator,
+        amount: Money.new(1000, "USD"),
+      )
+      FactoryBot.create(:donation,
+        donator: donator,
+        amount: Money.new(1000, "GBP"),
+      )
+      FactoryBot.create(:donation,
+        donator: donator,
+        amount: Money.new(1000, "EUR"),
+      )
+    end
+
+    it "sums all donations into the primary currency" do
+      # TODO: Make this adapt with changing exchange rates
+      expect(donator.total_donations).to eq(Money.new(2585, "GBP"))
+    end
+  end
+
+  describe "#hmac" do
+    let(:sha256_digest) { OpenSSL::Digest.new("SHA256") }
+    let(:hmac_secret) { SecureRandom.uuid }
+
+    around do |example|
+      with_env("HMAC_SECRET" => hmac_secret) do
+        example.run
+      end
+    end
+
+    it "encodes the user's ID with SHA256" do
+      expect(donator.hmac).to eq(
+        OpenSSL::HMAC.new(hmac_secret, sha256_digest)
+          .update(donator.id.to_s).hexdigest
+      )
+    end
+  end
+end
