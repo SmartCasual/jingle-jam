6ff0db918710af2f1e6effa70517d708b68361dd
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2021-03-25 14:05:31 +0000

    Fix migration naming

--- db/migrate/20210325113403_add_otp_to_admin_user.rb
+++ db/migrate/20210325113403_add_otp_to_admin_user.rb
@@ -1,4 +1,4 @@
-class AddOtpToAdminUser < ActiveRecord::Migration[6.1]
+class AddOTPToAdminUser < ActiveRecord::Migration[6.1]
   def change
     add_column :admin_users, :otp_secret, :string
     add_column :admin_users, :last_otp_at, :datetime
