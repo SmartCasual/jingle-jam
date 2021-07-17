c5e4e5c8bd2fea8b963aad6eef66ac200e5ba945
Ignacio Tolstoy <arkantos798@gmail.com>
2021-07-17 16:44:07 +0200

    Admin user requires a name

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.development?
   AdminUser.create!(
+    name: "Admin",
     email: "admin@example.com",
     password: "password",
     password_confirmation: "password",
