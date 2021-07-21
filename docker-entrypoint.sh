52db0af2bcc0ffae1127bb9d02f9ca19e5efd825
Jacob Berdichevsky <berdichevsky.jacob76@gmail.com>
2021-07-20 22:45:40 -0400

    Can now access the site kind of

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # bundle install --jobs 20 --retry 5
 
-# rails db:create
-# rails db:migrate
-# rails db:seed
+rails db:create
+rails db:migrate
+rails db:seed
 
 FROM_EMAIL_ADDRESS=jingle-jam@example.com
-HMAC_SECRET=some_secret_here
+HMAC_SECRET=c360bcae-df63-4616-808a-860f03d7da6b
 # gem install foreman 
-RAILS_ENV=development bundle exec rails s
+rm -f /jinglejam/tmp/pids/server.pid
+RAILS_ENV=development bundle exec rails server -b 0.0.0.0 -p 3000
