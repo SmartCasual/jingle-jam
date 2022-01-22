c1cd02b88f4277611cafc0e68c0f271e80954785
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-01-22 14:36:15 +0000

    ActiveAdmin autoloading fix

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module JingleJam
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths -= ["#{config.root}/app/admin"]
 
     # Don't generate system test files.
     config.generators.system_tests = nil
