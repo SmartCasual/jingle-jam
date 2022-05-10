6e11bc584ae18a907da09e59d642f19e2f42c54e
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-05-10 18:36:53 +0100

    Upgrade dependencies

--- features/support/chrome.rb
+++ features/support/chrome.rb
@@ -1,15 +1,17 @@
 Capybara.register_driver(:selenium_chrome_devtools) do |app|
-  driver = Capybara.drivers[:selenium_chrome].call
+  if (driver = Capybara.drivers[:selenium_chrome].call).nil?
+    raise "Cannot find selenium_chrome driver"
+  end
 
-  browser_options = driver.options[:options]
+  browser_options = driver.options.fetch(:capabilities)
   if (ENV.fetch("HEADLESS", nil) || ENV.fetch("HL", nil)) == "false"
     browser_options.args << "--auto-open-devtools-for-tabs" if ENV.fetch("DEVTOOLS", nil) && ENV.fetch("DEVTOOLS", nil) != "false"
   else
     browser_options.args << "--window-size=1920,1080"
-    browser_options.headless!
+    browser_options.args << "--headless"
   end
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: browser_options)
 end
 
 Capybara.default_driver = :selenium_chrome_devtools
